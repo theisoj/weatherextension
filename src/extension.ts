14055dc2b9e4299a2d3e916395711c8895596188
Jesse Keskelä <monkeydie_@hotmail.com>
2023-01-17 07:56:23 +0000

    lisää

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,16 @@ import * as vscode from 'vscode';
 import { getWeather } from './utils/getWeather.js';
 
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	let disposable = vscode.commands.registerCommand('weatherextension.getWeather', () => {
 
-		navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+		const paneeli = vscode.window.createWebviewPanel("openWeather", "Säätiedot", vscode.ViewColumn.One, { enableScripts: true })
 
-		function positionSuccess({ coords }: GeolocationPosition) {
-		   getWeather(coords.latitude, coords.longitude, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(e => {
-			   console.error(e)
-			   vscode.window.showErrorMessage("Virhe haettassa säätietoja")
-		  })
-		}
+		paneeli.webview.asWebviewUri(vscode.Uri.parse(""))
 
-    function positionError({ message }: GeolocationPositionError) {
-			vscode.window.showErrorMessage(`Virheilmoitus: ${message}`)
-		}
+	})
 
-  })
-
-  context.subscriptions.push(disposable)
+	context.subscriptions.push(disposable)
 }
 
 function renderWeather({ current }: RenderWeatherProps) {
@@ -28,7 +19,7 @@ function renderWeather({ current }: RenderWeatherProps) {
 }
 
 async function renderCurrentWeather(current: Current) {
-  vscode.window.showInformationMessage(`Nykyinen lämpötila on ${current.currentTemp} °C.`)	
+	vscode.window.showInformationMessage(`Nykyinen lämpötila on ${current.currentTemp} °C.`)
 }
 
-export function deactivate() {}
+export function deactivate() { }
